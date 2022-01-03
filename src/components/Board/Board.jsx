e7c11c9503b390a1c688416c5454709769fdbe21
Kostya Kaganov <kkaganov@gmail.com>
2022-01-03 21:11:18 +0200

    Lifting state up

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,35 +1,58 @@
+import { bind } from "bluebird";
 import React, { Component } from "react";
 import Square from "../Square/Square";
 
 class Board extends Component {
-  renderSquare(i) {
-    return <Square value={i} />;
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+        this.handleSquareClick = this.handleSquareClick.bind(this);
+    }
 
-  render() {
-    const status = `Next player: X`;
+    handleSquareClick(i) {
+        console.log(`Square ${i} was clicked`);
 
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
+        const squares = [...this.state.squares];
+        squares[i] = `X`;
+
+        this.setState({ squares });
+    }
+
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onSquareClick={() => this.handleSquareClick(i)}
+            />
+        );
+    }
+
+    render() {
+        const status = `Next player: X`;
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Board;
--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,20 +1,10 @@
 import React, { Component } from "react";
 
 class Square extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
     render() {
         return (
-            <button
-                className="square"
-                onClick={() => this.setState({ value: `X` })}
-            >
-                {this.state.value}
+            <button className="square" onClick={this.props.onSquareClick}>
+                {this.props.value}
             </button>
         );
     }
