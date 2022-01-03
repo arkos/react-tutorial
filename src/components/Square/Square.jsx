86922a397d7ad7662266db96e68f44c74239a162
Kostya Kaganov <kkaganov@gmail.com>
2022-01-03 20:48:22 +0200

    Format after .editorconfig

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 
 class Square extends Component {
-  render() {
-    return (
-      <button className="square" onClick={() => console.log(this.props.value)}>
-        {this.props.value}
-      </button>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
+    render() {
+        return (
+            <button
+                className="square"
+                onClick={() => this.setState({ value: `X` })}
+            >
+                {this.state.value}
+            </button>
+        );
+    }
 }
 
 export default Square;
